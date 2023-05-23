ce4a612b5bb82b18779a5c1a89ab49289e9151dc
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-23 16:23:44 +0300

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 export const App: React.FC = () => {
-  return <div>Hello</div>;
-}
+  return (
+    <>
+      <div>Hello</div>
+      <Outlet />
+    </>
+  );
+};
 
 export default App;
