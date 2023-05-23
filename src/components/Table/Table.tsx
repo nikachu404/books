0eb791dcc3c9750c9efa8bd62edbf62f339a58a4
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-23 18:48:33 +0300

    chanded date format, added books fetching

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import './Table.scss';
+import moment from 'moment';
+import { Book } from '../../types/Book';
 
-export const Table: React.FC = () => {
-  const data = [
-    {
-      id: 1,
-      title: 'Book 1',
-      author: 'Author 1',
-      category: 'Category 1',
-      isbn: '1234567890',
-      createdAt: new Date(2022, 2, 12, 8, 35),
-      modifiedAt: new Date(2022, 2, 13, 13, 48),
-    },
-    {
-      id: 2,
-      title: 'Book 2',
-      author: 'Author 2',
-      category: 'Category 2',
-      isbn: '0987654321',
-      createdAt: new Date(2022, 4, 20, 11, 15),
-      modifiedAt: new Date(2022, 4, 21, 9, 30),
-    },
-  ];
+type Props = {
+  books: Book[];
+};
 
+export const Table: React.FC<Props> = ({ books }) => {
   return (
     <table className="table">
       <thead>
@@ -37,31 +22,15 @@ export const Table: React.FC = () => {
         </tr>
       </thead>
       <tbody>
-        {data.map(item => (
+        {books.map(item => (
           <tr key={item.id}>
             <td>{item.title}</td>
             <td>{item.author}</td>
             <td>{item.category}</td>
             <td>{item.isbn}</td>
+            <td>{moment(item.createdAt).format('LL LT')}</td>
             <td>
-              {item.createdAt.toLocaleString('en-US', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric',
-                hour: 'numeric',
-                minute: 'numeric',
-                hour12: true,
-              })}
-            </td>
-            <td>
-              {item.modifiedAt.toLocaleString('en-US', {
-                day: 'numeric',
-                month: 'long',
-                year: 'numeric',
-                hour: 'numeric',
-                minute: 'numeric',
-                hour12: true,
-              })}
+              {item.modifiedAt ? moment(item.modifiedAt).format('LL LT') : '--'}
             </td>
             <td>
               <button>Edit</button>
