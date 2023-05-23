d2aa56879d10e0e9d3b323d6aba3fb4af09e1606
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-23 22:09:54 +0300

    added toggle function

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,14 +9,31 @@ export const Dashboard: React.FC = () => {
 
   useEffect(() => {
     loadBooks();
-  }, []);
+  }, [filter]);
 
   const loadBooks = () => {
-    axios.get('http://localhost:3000/books').then(res => setBooks(res.data));
+    let url = 'http://localhost:3000/books';
+
+    if (filter === 'active') {
+      url += '?isActive=true';
+    } else if (filter === 'deactivated') {
+      url += '?isActive=false';
+    }
+
+    axios.get(url).then(res => setBooks(res.data));
   };
 
   const deleteBook = (id: number) => {
     axios.delete(`http://localhost:3000/books/${id}`).then(() => loadBooks());
+  };
+
+  const toggleActivation = (book: Book, isActive: boolean) => {
+    axios
+      .put(`http://localhost:3000/books/${book.id}`, {
+        ...book,
+        isActive: isActive,
+      })
+      .then(() => loadBooks());
   };
 
   const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -31,7 +48,11 @@ export const Dashboard: React.FC = () => {
         <option value="deactivated">Show Deactivated</option>
       </select>
 
-      <Table books={books} onDeleteBook={deleteBook} />
+      <Table
+        books={books}
+        onDeleteBook={deleteBook}
+        onToggleActive={toggleActivation}
+      />
     </>
   );
 };
