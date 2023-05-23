774ba218447d2bf6ce05132b8b8314c7b4d931b3
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-23 21:07:33 +0300

    added bookForm component

---
+++ src/components/BookForm/BookForm.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Book } from '../../types/Book';
+
+export const BookForm: React.FC = () => {
+  const { id } = useParams();
+
+  const [book, setBook] = useState<Book>({
+    id: 0,
+    title: '',
+    author: '',
+    category: '',
+    isbn: '',
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+  });
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`http://localhost:3000/books/${id}`)
+        .then(res => setBook(res.data));
+    }
+  }, [id]);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const updatedBook = {
+      ...book,
+      modifiedAt: new Date(),
+    };
+
+    if (id) {
+      axios.put(`http://localhost:3000/books/${id}`, updatedBook);
+    } else {
+      axios.post('http://localhost:3000/books', updatedBook);
+    }
+
+    setBook({
+      id: 0,
+      title: '',
+      author: '',
+      category: '',
+      isbn: '',
+      createdAt: new Date(),
+      modifiedAt: new Date(),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Book title</label>
+        <input
+          type="text"
+          name="title"
+          value={book.title}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Author name</label>
+        <input
+          type="text"
+          name="author"
+          value={book.author}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Category</label>
+        <select
+          name="category"
+          value={book.category}
+          onChange={handleChange}
+          required>
+          <option value="">Select a category</option>
+          <option value="Category 1">Category 1</option>
+          <option value="Category 2">Category 2</option>
+          <option value="Category 3">Category 3</option>
+        </select>
+      </div>
+      <div>
+        <label>ISBN</label>
+        <input
+          type="number"
+          name="isbn"
+          value={book.isbn}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">{id ? 'Edit Book' : 'Add a Book'}</button>
+    </form>
+  );
+};
