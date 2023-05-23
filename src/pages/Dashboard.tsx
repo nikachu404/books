28c5388e84d2b9bb99a7812e2ccf0fe7256a1b0f
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-23 18:02:40 +0300

    added filter selector

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Table } from '../components/Table/Table';
 
 export const Dashboard: React.FC = () => {
+  const [filter, setFilter] = useState('active');
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(e.target.value);
+  };
+
   return (
-    <div>Dashboard</div>
+    <>
+      <select id="filter" value={filter} onChange={handleFilterChange}>
+        <option value="all">Show All</option>
+        <option value="active">Show Active</option>
+        <option value="deactivated">Show Deactivated</option>
+      </select>
+
+      <Table />
+    </>
   )
 }
