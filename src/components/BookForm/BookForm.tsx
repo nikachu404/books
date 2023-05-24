994aa5333bd3cd00b4b6ef4f0948e69c35c50ca4
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-24 19:51:51 +0300

    added info message

--- src/components/BookForm/BookForm.tsx
+++ src/components/BookForm/BookForm.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Book } from '../../types/Book';
+
+import './BookForm.scss';
 
 export const BookForm: React.FC = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
+  const [message, setMessage] = useState('');
   const [book, setBook] = useState<Book>({
     id: 0,
     title: '',
@@ -33,7 +37,7 @@ export const BookForm: React.FC = () => {
     });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const requestOptions: RequestInit = {
@@ -42,11 +46,28 @@ export const BookForm: React.FC = () => {
       body: JSON.stringify(
         id
           ? { ...book, editedAt: new Date() }
-          : { ...book, createdAt: new Date() }
+          : { ...book, createdAt: new Date() },
       ),
     };
 
-    fetch(`http://localhost:3000/books${id ? `/${id}` : ''}`, requestOptions);
+    try {
+      const response = await fetch(
+        `http://localhost:3000/books${id ? `/${id}` : ''}`,
+        requestOptions,
+      );
+
+      if (response.ok) {
+        setMessage('Record saved successfully!');
+        setTimeout(() => {
+          navigate('/');
+        }, 2000);
+      } else {
+        setMessage('Failed to save record.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setMessage('An error occurred. Please try again.');
+    }
 
     setBook({
       id: 0,
@@ -61,35 +82,44 @@ export const BookForm: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
+    <form onSubmit={handleSubmit} className="book-form">
+      <div className="book-form__field">
         <label>Book title</label>
         <input
           type="text"
           name="title"
           value={book.title}
           onChange={handleChange}
+          autoComplete="off"
           required
+          className="book-form__input"
         />
       </div>
-      <div>
+
+      <div className="book-form__field">
         <label>Author name</label>
         <input
           type="text"
           name="author"
           value={book.author}
           onChange={handleChange}
+          autoComplete="off"
           required
+          className="book-form__input"
         />
       </div>
-      <div>
+
+      <div className="book-form__field">
         <label>Category</label>
         <select
           name="category"
           value={book.category}
           onChange={handleChange}
-          required>
-          <option value="" disabled>Select a category</option>
+          required
+          className="book-form__input">
+          <option value="" disabled>
+            Select a category
+          </option>
           <option value="Fantasy">Fantasy</option>
           <option value="Science Fiction">Science Fiction</option>
           <option value="Mystery">Mystery</option>
@@ -97,17 +127,23 @@ export const BookForm: React.FC = () => {
           <option value="Thriller">Thriller</option>
         </select>
       </div>
-      <div>
+
+      <div className="book-form__field">
         <label>ISBN</label>
         <input
           type="number"
           name="isbn"
           value={book.isbn}
           onChange={handleChange}
+          autoComplete="off"
           required
+          className="book-form__input"
         />
       </div>
-      <button type="submit">{id ? 'Edit Book' : 'Add a Book'}</button>
+      <button type="submit" className="book-form__submit-button">
+        {id ? 'Edit Book' : 'Add a Book'}
+      </button>
+      {message && <p className="book-form__message">{message}</p>}
     </form>
   );
 };
