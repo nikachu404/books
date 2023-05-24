f72f74d28c2f9508a68ff1774f8418238476b12e
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-24 19:08:04 +0300

    added dashboard styles

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Table } from '../../components';
+import { Book } from '../../types/Book';
+import { PLUS_ICON } from '../../constants';
+
+import './Dashboard.scss';
+
+export const Dashboard: React.FC = () => {
+  const [filter, setFilter] = useState('active');
+  const [books, setBooks] = useState<Book[]>([]);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [filteredRecords, setFilteredRecords] = useState(0);
+
+  useEffect(() => {
+    loadBooks();
+  }, [filter]);
+
+  const loadBooks = () => {
+    let url = 'http://localhost:3000/books';
+
+    if (filter === 'active') {
+      url += '?isActive=true';
+    } else if (filter === 'deactivated') {
+      url += '?isActive=false';
+    }
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setBooks(data);
+        setFilteredRecords(data.length);
+      });
+
+    fetch('http://localhost:3000/books')
+      .then(response => response.json())
+      .then(data => setTotalRecords(data.length));
+  };
+
+  const deleteBook = (id: number) => {
+    fetch(`http://localhost:3000/books/${id}`, { method: 'DELETE' }).then(() =>
+      loadBooks(),
+    );
+  };
+
+  const toggleActivation = (book: Book, isActive: boolean) => {
+    fetch(`http://localhost:3000/books/${book.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...book, isActive: isActive }),
+    }).then(() => loadBooks());
+  };
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(e.target.value);
+  };
+
+  return (
+    <div className="dashboard">
+      <div className="dashboard__filter">
+        <select
+          id="filter"
+          value={filter}
+          onChange={handleFilterChange}
+          className="dashboard__filter-select">
+          <option value="all">Show All</option>
+          <option value="active">Show Active</option>
+          <option value="deactivated">Show Deactivated</option>
+        </select>
+
+        <p className="dashboard__filter-info">
+          Showing {filteredRecords} of {totalRecords} records.
+        </p>
+      </div>
+
+      <Table
+        books={books}
+        onDeleteBook={deleteBook}
+        onToggleActive={toggleActivation}
+      />
+
+      <Link to={'/add'} className="dashboard__add-link">
+        <div className="dashboard__add-button">
+          <img src={PLUS_ICON} alt="add" className="dashboard__add-plus" />
+        </div>
+      </Link>
+    </div>
+  );
+};
