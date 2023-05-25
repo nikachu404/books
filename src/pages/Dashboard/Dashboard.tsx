c9132a4e58a2091d04b72af1f94ab89148f8c41f
nikachu404 <yasinskaa.veronika@gmail.com>
2023-05-25 14:14:16 +0300

    changed promises to async await

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -17,7 +17,7 @@ export const Dashboard: React.FC = () => {
     loadBooks();
   }, [filter]);
 
-  const loadBooks = () => {
+  const loadBooks = async () => {
     let url = API_URL;
 
     if (filter === 'active') {
@@ -26,47 +26,55 @@ export const Dashboard: React.FC = () => {
       url += '?isActive=false';
     }
 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        setBooks(data);
-        setFilteredRecords(data.length);
-        setFetchSuccess(true);
-      })
-      .catch(() => {
-        setBooks([]);
-        setFilteredRecords(0);
-        setFetchSuccess(false);
-      });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setBooks(data);
+      setFilteredRecords(data.length);
+      setFetchSuccess(true);
+    } catch (error) {
+      setBooks([]);
+      setFilteredRecords(0);
+      setFetchSuccess(false);
+    }
 
-    fetch(API_URL)
-      .then(response => response.json())
-      .then(data => {
-        setTotalRecords(data.length);
-        setFetchSuccess(true);
-      })
-      .catch(() => {
-        setFilteredRecords(0);
-        setFetchSuccess(false);
-      });
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setTotalRecords(data.length);
+      setFetchSuccess(true);
+    } catch (error) {
+      setFilteredRecords(0);
+      setFetchSuccess(false);
+    }
   };
 
-  const deleteBook = (id: number) => {
-    fetch(`${API_URL}/${id}`, { method: 'DELETE' }).then(() => loadBooks());
-  };
-
-  const toggleActivation = (book: Book, isActive: boolean) => {
-    fetch(`${API_URL}/${book.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ ...book, isActive: isActive }),
-    }).then(() => loadBooks());
+  const toggleActivation = async (book: Book, isActive: boolean) => {
+    try {
+      fetch(`${API_URL}/${book.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...book, isActive: isActive }),
+      });
+      await loadBooks();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setFilter(e.target.value);
+  };
+
+  const deleteBook = async (id: number) => {
+    try {
+      await fetch(`${API_URL}/${id}`, { method: 'DELETE' });
+      await loadBooks();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
